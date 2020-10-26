2b1e8138739e9f16f9e40db191ac1c37a90183cc
annavukovich <acvukovich@stu.naperville203.org>
2020-10-26 13:11:11 -0500

    output for minivan

--- Chapter3B Starter Code/Minivan.java
+++ Chapter3B Starter Code/Minivan.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 /**
  * Write a description of class Minivan here.
@@ -8,7 +9,31 @@ import java.util.Scanner;
 public class Minivan
 {
     public static void main(String[] args){
-        System.out.println("Sequence: "); 
+        Scanner in = new Scanner(System.in); 
+        System.out.println("Sequence: DL DR CL MU IL IR OL OR Gear (with no spaces) "); 
+        String sequence=new String(in.nextLine()); 
+        boolean open=true, DashLeft=true, DashRight=true, inside=true; 
         
+        if (!(sequence.substring(8).equals("P") ))
+            open= false;
+        else if (sequence.substring(3,4).equals("0"))
+            inside= false; 
+        else if (sequence.substring(1,2).equals("0")){
+            DashRight= false;
+        }
+        else if (sequence.substring(0,1).equals("0")){
+            DashLeft= false; 
+        }
+        
+        if (open==false)
+            System.out.println("Both doors stay closed");
+        else if (DashRight== false && DashLeft==false)
+            System.out.println("Bothe doors stay closed");
+        else if (DashRight != false && DashLeft !=false)
+            System.out.println("Both doors open"); 
+        else if (DashRight != false)
+            System.out.println("Right opens");
+        else if (DashLeft != false)
+            System.out.println("Left opens");
     }
 }
